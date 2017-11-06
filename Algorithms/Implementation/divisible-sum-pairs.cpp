8b959e6e6863acd9f39d1034096f46da87b260a2
Prateek Chaplot <prateekchaplot96@gmail.com>
2017-11-06 23:23:16 +0530

    Create divisible-sum-pairs.cpp

---
+++ Algorithms/Implementation/divisible-sum-pairs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin>>n>>k;
+    
+    int a[n];
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+    
+    int ways=0;
+    for(int i=0; i<n; i++)
+        for(int j=i+1; j<n; j++)
+            if((a[i]+a[j])%k == 0)
+                ways++;
+    
+    cout<<ways<<endl;
+    return 0;
+}
